c855f3367e7fecdc3d2ada84c3fefd57dc0278d0
ridhwan-saal <ridhwan@saal.ai>
2022-05-22 23:15:17 +0400

    Fix writing to volume

--- src/pipelines/volume_test/write.py
+++ src/pipelines/volume_test/write.py
@@ -5,6 +5,7 @@ import kfp.components
 
 def write_file(contents: str, path: str):
     import os
+    os.makedirs(path, exist_ok=True)
     full_path = os.path.join(path, "file.txt")
     with open(full_path, "w", encoding='utf-8') as f:
         f.write(contents)
@@ -16,22 +17,25 @@ def read_file(path: str):
         return f.read()
 
 @kfp.dsl.pipeline(
-    name="Shared Volume Pipeline",
+    name="Writing Into PVC Test",
     description="Test writing to a consistent volume",
 )
-def write_volume_pipeline(write_path: str):
+def write_volume_pipeline():
     from kfp import dsl
 
     mount_vol = dsl.VolumeOp(
-        name="Create Volume", # Operation Name
-        size="4Gi",
+        name="Create Ridhwan Volumes", # Volume Unique Name, will be used as identifier to operation.
+                                               # If operation exists then it will be reused.
+                                               # If operation does not exist then it will be created and the name will be resource_name below.
+        size="1Gi",
         modes=dsl.VOLUME_MODE_RWO,
-        resource_name="shared-volume-ridhwan-resource", # PVC Name
-        volume_name="shared-volume-ridhwan-volume", # Volume Name
-        # data_source=PipelineParam(name="data_source"), # This way it will create a volume snapshot from param, which is probably what we need?
+        resource_name="ridhwan-pvc-mount", # PVC Name, will be {pipeline_name}-{id}-resource if name does not exist and generate_unique_name is True.
+        # volume_name="ridhwan-personal-volume", # Volume Name, do not use as it will cause the volume to not be found
+        # data_source=PipelineParam(name="data_source"), # This way it will create a volume snapshot from param
+        generate_unique_name=False,
     )
 
-    data_store_path = write_path
+    data_store_path = "/data"
     pvolumes = {data_store_path: mount_vol.volume}
     volumetrize = lambda func: func.add_pvolumes(pvolumes)
 
@@ -39,6 +43,6 @@ def write_volume_pipeline(write_path: str):
     write_volume_component = kfp.components.create_component_from_func(write_file)
     read_volume_component = kfp.components.create_component_from_func(read_file)
     # Tasks
-    write_task = volumetrize(write_volume_component("This is a file created by the pipline " + str(write_path), data_store_path))
+    write_task = volumetrize(write_volume_component("This is a file created by the pipeline into {}".format("/data"), data_store_path))
     read_task = volumetrize(read_volume_component(data_store_path))
     read_task.after(write_task)
