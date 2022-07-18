34318d366f7f5f7849f073742883f2650f49340f
ridhwan-saal <ridhwan@saal.ai>
2022-07-18 18:42:11 +0400

    Add handler for HF task (WIP)

---
+++ src/handlers/topic_class.py
@@ -0,0 +1,162 @@
+# My dependencies
+from fastapi import FastAPI
+import uvicorn
+from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from transformers import AutoConfig, AutoTokenizer, AutoModelForSequenceClassification
+import anltk
+import unicodedata
+import torch
+
+# Guide dependencies
+from abc import ABC
+import logging
+import torch
+from ts.torch_handler.base_handler import BaseHandler
+
+logger = logging.getLogger(__name__)
+
+class TopicClassifier(BaseHandler, ABC):
+    def __init__(self,):
+        super().__init__()
+        self.initialized = False
+
+    def initialize(self, ctx):
+        self.manifest = ctx.manifest
+
+        properties = ctx.system_properties
+        model_path = properties.get("model_dir")
+        self.device = torch.device("cuda:" + str(properties.get("gpu_id")) if torch.cuda.is_available() else "cpu")
+
+        self.config = AutoConfig.from_pretrained(model_path)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path, config=self.config)
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_path, config=self.config)
+
+        self.model.to(self.device)
+        self.model.eval()
+
+        # Optional Mapping File
+        self.labels = list(self.config.label2id.keys())
+
+
+        logger.log(logging.INFO, f"Initialized Topic Classifier")
+        self.initialized = True
+
+    def preprocess_(self, query: str) -> str:
+        query_ = unicodedata.normalize('NFC', query)
+        query_ = ' '.join(anltk.tokenize_words(query_))
+        query_ = anltk.remove_non_alpha(query_, stop_list=' ?,:".')
+        query_ = anltk.fold_white_spaces(query_)
+        query_ = query_.strip()
+        return query_
+
+    def preprocess(self, data):
+        query = data[0].get("data")
+        if query is None:
+            query = data[0].get("body")
+
+        query_ = self.preprocess(query)
+
+        # TODO: These are used only for display, remove in production
+        tokens = self.tokenizer.tokenize(query_)
+
+        encoded_dict = self.tokenizer.encode_plus(
+                        query_,                      # Sentence to encode.
+                        add_special_tokens = True, # Add '[CLS]' and '[SEP]'
+                        max_length=512,           # Pad & truncate all sentences.
+                        truncation=True,
+                        padding='max_length',  # Padding strategy
+                        pad_to_max_length = True,
+                        return_attention_mask = True,   # Construct attn. masks.
+                        return_tensors = 'pt',     # Return pytorch tensors.
+                    )
+        return encoded_dict, tokens, query_
+    
+    def inference(self, inputs,):
+        with torch.no_grad():
+            for key in inputs: # Convert all to device first
+                try: 
+                    inputs[key] = inputs[key].to(self.device)
+                except:
+                    pass
+            outputs = self.model(**inputs)
+        
+        predictions = torch.nn.functional.softmax(outputs[0].squeeze(), dim=0)
+        pred = torch.argmax(predictions, dim=0)
+
+        correct = self.labels[pred.item()]
+
+        logger.log(logging.INFO, f"Predicted: {correct}")
+
+        labeled_dict = {"Correct": correct}
+        for label in self.labels:
+            labeled_dict[label] = "{:.3f}".format(predictions[self.config.label2id[label]].item())
+        
+        return labeled_dict
+        # return {
+        #     "Culture": f'{predictions[self.config.label2id["Culture"]].item():.3f}',
+        #     "Religion": f'{predictions[self.config.label2id["Religion"]].item():.3f}',
+        #     "Sports": f'{predictions[self.config.label2id["Sports"]].item():.3f}',
+        #     "Tech": f'{predictions[self.config.label2id["Tech"]].item():.3f}',
+        #     "Oil": f'{predictions[self.config.label2id["Oil"]].item():.3f}',
+        #     "Finance": f'{predictions[self.config.label2id["Finance"]].item():.3f}',
+        #     "Medical": f'{predictions[self.config.label2id["Medical"]].item():.3f}',
+        #     "Politics": f'{predictions[self.config.label2id["Politics"]].item():.3f}',
+        #     "Correct": self.labels[pred.item()],
+        #     "tokens": tokens,
+        #     "preprocessed": query_processed
+        # }
+
+    def postprocess(self, data: dict, tokens, query):
+        data["Tokens"] = tokens
+        data["Preprocessed"] = query
+        return [data] # Return the data as is but in a list
+
+_service = TopicClassifier()
+
+def handle(data, context):
+    try:
+        if not _service.initialized:
+            _service.initialize(context)
+        
+        if data is None:
+            return None
+        
+        inputs, tokens, query = _service.preprocess(data)
+        output_dict = _service.inference(inputs)
+        outputs = _service.postprocess(output_dict, tokens, query)
+
+        return outputs
+    except Exception as e:
+        logger.error(e)
+        raise e
+
+# model_path = '/storage/tmp/topic_classifcation'
+# topic_model = TopicClassifier(model_path)
+# app = FastAPI()
+
+# origins = [
+#     "http://localhost",
+#     "http://localhost:8000",
+#     "http://http://192.168.71.54:8000"
+# ]
+# origins = ["*"]
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# class SentimentItem(BaseModel):
+#     query: str
+
+# @app.post("/classify_topic/")
+# async def classify_topic(item: SentimentItem):
+#     print('Info : {}'.format(item.query))
+#     return topic_model(item.query)
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=7100, reload=False)
--- src/pipelines/end_to_end/serve_pt.py
+++ src/pipelines/end_to_end/serve_pt.py
@@ -11,13 +11,11 @@ from src.pipelines.common_utils import (add_pvolumes_func, get_volume_by_name, s
 
 def create_serve_task(dataset_name, experiment_name, mount_name):
 
-    gen_name_comp = func_to_container_op(generate_inference_service_name)
-    gen_name_task = gen_name_comp(dataset_name, experiment_name)
-    model_name = gen_name_task.output
+    # gen_name_comp = func_to_container_op(generate_inference_service_name)
+    # gen_name_task = gen_name_comp(dataset_name, experiment_name)
     
     infer_service = spec_from_file_format(
         "src/pipelines/yamls/Specs/KServe_HF.yaml",
-        modelName=model_name,
         modelNamespace="{{workflow.namespace}}",
         volumeResourceName=mount_name,
         experimentName=experiment_name,
@@ -30,17 +28,79 @@ def create_serve_task(dataset_name, experiment_name, mount_name):
         inferenceservice_yaml=yaml.dump(infer_service),
     )
     
-    return serve_task, model_name
+    # return serve_task, service_name
+    return serve_task
 
 def generate_inference_service_name(dataset_name, experiment_name) -> str:
     return "{}-{}".format(dataset_name, experiment_name).lower().replace('_', '-')
 
+def create_handler(handler_code: str) -> str:
+    with open("/tmp/handler.py", "w") as f:
+        f.write(handler_code)
+
+    return "/tmp/handler.py"
+
+def convert_model_to_mar(model_name: str, experiment_name: str, root_path: str) -> str:
+    import os
+    # This should be a separate function, one to move he files and so on, and one to do the task
+
+
+def get_mar_required_files(experiment_name: str, dataset_name: str, root_path: str) -> NamedTuple(
+    "Outputs", [
+        ("model_path", str),
+        ("extra_files", str)
+    ]
+):
+    import os
+    # storageUri: "pvc://{volumeResourceName}/{experimentName}/outputs/{datasetName}"
+    model_folder = os.path.join(root_path, experiment_name, "outputs", dataset_name)
+    model = os.path.join(model_folder, "pytorch_model.bin")
+    vocab = os.path.join(model_folder, "vocab.txt")
+    config = os.path.join(model_folder, "config.json")
+
+    return model, "{},{}".format(vocab, config)
+
 @kfp.dsl.pipeline(
-    name="End to End Hugging Face Topic Classifier",
-    description="End to End Topic Classiciation using HuggingFace Framework and CamelBert model"
+    name="End to End Hugging Face Topic Classifier - Serving",
+    description="The Serving part of the E2E HF Topic Classifier - DEBUG ONLY"
 )
-def pipeline(experiment_name: str, volume_name: str, dataset_name: str): 
+def pipeline(experiment_name: str, volume_name: str, dataset_name: str, model_version: str = "0.0.1"): 
     volumetrize = setup_volume(volume_name, "/store")
 
     serve_task, service_name = create_serve_task(dataset_name, experiment_name, volume_name)
     serve_task = volumetrize(serve_task)
+
+    # Convert this to a utility function
+    handler_path = "src/handlers/topic_class.py"
+    handler_code = open(handler_path, encoding='utf-8').read()
+
+    handler_op = components.func_to_container_op(create_handler)
+    handler_task = handler_op(handler_code)
+    handler_task = volumetrize(handler_task) # To allow saving
+
+    get_mar_op = components.func_to_component_op(get_mar_required_files)
+    get_mar_task = get_mar_op(experiment_name, dataset_name, "/store")
+
+    model_path = get_mar_task.outputs.model_path
+    extra_files = get_mar_task.outputs.extra_files
+    model_name = dataset_name
+
+    convertor_op = components.create_component_from_func(
+        convert_model_to_mar,
+        base_image="python:3.7",
+        packages_to_install=['transformers[torch]', 'fastapi[standard]', 'uvicorn[standard]', 'anltk', "torch-model-archiver"],
+        command=["torch-model-archiver"],
+        args=[ # If this doesn't work then create a component yaml for it
+            "--model-name", model_name,
+            "--version", model_version,
+            "--serialized-file", model_path,
+            "--extra-files", extra_files,
+            "--handler", handler_path,
+        ]
+    )
+
+
+    # fastapi_deploy_op = kfp.components.create_component_from_func(
+	# 	fastapi_deploy,
+	# 	packages_to_install=['transformers[torch]', 'fastapi[standard]', 'uvicorn[standard]', 'anltk']
+	# )
