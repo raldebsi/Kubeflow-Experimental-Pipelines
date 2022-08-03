d8ca33bfc1d80bca3f18919c78189ab10edc6895
ridhwan-saal <ridhwan@saal.ai>
2022-08-03 23:55:27 +0400

    Fix handler reading part

--- src/handlers/topic_class.py
+++ src/handlers/topic_class.py
@@ -1,8 +1,3 @@
-# My dependencies
-# from fastapi import FastAPI
-# import uvicorn
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
 from transformers import AutoConfig, AutoTokenizer, AutoModelForSequenceClassification
 import anltk
 import unicodedata
@@ -36,11 +31,11 @@ class TopicClassifier(BaseHandler, ABC):
         self.model.to(self.device)
         self.model.eval()
 
-        # Optional Mapping File
+        # Token to Label
         self.labels = list(self.config.label2id.keys())
 
 
-        logger.log(logging.INFO, f"Initialized Topic Classifier")
+        logger.log(logging.INFO, "=============TOPIC CLASSIFIER INITIALIZED=============")
         self.initialized = True
 
     def preprocess_(self, query: str) -> str:
@@ -52,14 +47,18 @@ class TopicClassifier(BaseHandler, ABC):
         return query_
 
     def preprocess(self, data):
-        query = data[0].get("data")
-        if query is None:
-            query = data[0].get("body")
+        logger.log(logging.INFO, f"Preprocessing started")
+        logger.log(logging.INFO, f"data is {data}")
+        query = data[0]
+        query = query.get("body", {"text": query.get("text", "")}).get("text", "")
+        if not query.strip():
+            raise Exception("No text found in query")
+        query = self.preprocess_(query)
+        logger.log(logging.INFO, f"query is {query}")
 
-        query_ = self.preprocess(query)
+        query_ = self.preprocess_(query)
 
-        # TODO: These are used only for display, remove in production
-        tokens = self.tokenizer.tokenize(query_)
+        # tokens = self.tokenizer.tokenize(query_) # Debugging only
 
         encoded_dict = self.tokenizer.encode_plus(
                         query_,                      # Sentence to encode.
@@ -71,7 +70,8 @@ class TopicClassifier(BaseHandler, ABC):
                         return_attention_mask = True,   # Construct attn. masks.
                         return_tensors = 'pt',     # Return pytorch tensors.
                     )
-        return encoded_dict, tokens, query_
+        # return encoded_dict, tokens, query_
+        return encoded_dict, query_
     
     def inference(self, inputs,):
         logger.log(logging.INFO, f"Inference started")
@@ -90,28 +90,15 @@ class TopicClassifier(BaseHandler, ABC):
 
         logger.log(logging.INFO, f"Predicted: {correct}")
 
-        labeled_dict = {"Correct": correct}
+        class_dict = {}
+        labeled_dict = {"Correct": correct, "Classes": class_dict}
         for label in self.labels:
-            labeled_dict[label] = "{:.3f}".format(predictions[self.config.label2id[label]].item())
+            class_dict[label] = "{:.3f}".format(predictions[self.config.label2id[label]].item())
         
         return labeled_dict
-        # return {
-        #     "Culture": f'{predictions[self.config.label2id["Culture"]].item():.3f}',
-        #     "Religion": f'{predictions[self.config.label2id["Religion"]].item():.3f}',
-        #     "Sports": f'{predictions[self.config.label2id["Sports"]].item():.3f}',
-        #     "Tech": f'{predictions[self.config.label2id["Tech"]].item():.3f}',
-        #     "Oil": f'{predictions[self.config.label2id["Oil"]].item():.3f}',
-        #     "Finance": f'{predictions[self.config.label2id["Finance"]].item():.3f}',
-        #     "Medical": f'{predictions[self.config.label2id["Medical"]].item():.3f}',
-        #     "Politics": f'{predictions[self.config.label2id["Politics"]].item():.3f}',
-        #     "Correct": self.labels[pred.item()],
-        #     "tokens": tokens,
-        #     "preprocessed": query_processed
-        # }
 
-    def postprocess(self, data: dict, tokens, query):
-        data["Tokens"] = tokens
-        data["Preprocessed"] = query
+    def postprocess(self, data: dict, query):
+        # data["Preprocessed"] = query # No Need
         return [data] # Return the data as is but in a list
 
 _service = TopicClassifier()
@@ -124,40 +111,13 @@ def handle(data, context):
         if data is None:
             return None
         
-        inputs, tokens, query = _service.preprocess(data)
+        logger.log(logging.INFO, f"Received data: {data}")
+        
+        inputs, query = _service.preprocess(data)
         output_dict = _service.inference(inputs)
-        outputs = _service.postprocess(output_dict, tokens, query)
+        outputs = _service.postprocess(output_dict, query)
 
         return outputs
     except Exception as e:
         logger.error(e)
         raise e
-
-# model_path = '/storage/tmp/topic_classifcation'
-# topic_model = TopicClassifier(model_path)
-# app = FastAPI()
-
-# origins = [
-#     "http://localhost",
-#     "http://localhost:8000",
-#     "http://http://192.168.71.54:8000"
-# ]
-# origins = ["*"]
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# class SentimentItem(BaseModel):
-#     query: str
-
-# @app.post("/classify_topic/")
-# async def classify_topic(item: SentimentItem):
-#     print('Info : {}'.format(item.query))
-#     return topic_model(item.query)
-
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=7100, reload=False)
