b309745da0bf3970aa1bfbdfeb288051662efe20
ridhwan-saal <ridhwan@saal.ai>
2022-10-04 12:01:00 +0400

    Update handler

--- src/handlers/topic_class.py
+++ src/handlers/topic_class.py
@@ -14,10 +14,10 @@ logger = logging.getLogger(__name__)
 class TopicClassifier(BaseHandler, ABC):
     def __init__(self,):
         super().__init__()
-        logger.log(logging.INFO, "=============INITIALIZING TOPIC CLASSIFIER=============")
         self.initialized = False
 
     def initialize(self, ctx):
+        logger.log(logging.INFO, "=============INITIALIZING TOPIC CLASSIFIER=============")
         self.manifest = ctx.manifest
 
         properties = ctx.system_properties
@@ -31,14 +31,17 @@ class TopicClassifier(BaseHandler, ABC):
         self.model.to(self.device)
         self.model.eval()
 
-        # Token to Label
+        # Label Mapping
         self.labels = list(self.config.label2id.keys())
 
 
-        logger.log(logging.INFO, "=============TOPIC CLASSIFIER INITIALIZED=============")
+        logger.log(logging.INFO, f"Initialized Topic Classifier")
         self.initialized = True
 
     def preprocess_(self, query: str) -> str:
+        query = query.strip()
+        if not query:
+            return ""
         query_ = unicodedata.normalize('NFC', query)
         query_ = ' '.join(anltk.tokenize_words(query_))
         query_ = anltk.remove_non_alpha(query_, stop_list=' ?,:".')
@@ -51,9 +54,9 @@ class TopicClassifier(BaseHandler, ABC):
         logger.log(logging.INFO, f"data is {data}")
         query = data[0]
         query = query.get("body", {"text": query.get("text", "")}).get("text", "")
+        query = self.preprocess_(query)
         if not query.strip():
             raise Exception("No text found in query")
-        query = self.preprocess_(query)
         logger.log(logging.INFO, f"query is {query}")
 
         query_ = self.preprocess_(query)
@@ -69,19 +72,17 @@ class TopicClassifier(BaseHandler, ABC):
                         pad_to_max_length = True,
                         return_attention_mask = True,   # Construct attn. masks.
                         return_tensors = 'pt',     # Return pytorch tensors.
-                    )
-        # return encoded_dict, tokens, query_
-        return encoded_dict, query_
+        )
+        return (encoded_dict, query_)
     
     def inference(self, inputs,):
-        logger.log(logging.INFO, f"Inference started")
         with torch.no_grad():
-            for key in inputs: # Convert all to device first
+            for key, val in inputs.items(): # Convert all to device first
                 try: 
-                    inputs[key] = inputs[key].to(self.device)
+                    inputs[key] = val.to(self.device)
                 except:
                     pass
-            outputs = self.model(**inputs)
+        outputs = self.model(**inputs)
         
         predictions = torch.nn.functional.softmax(outputs[0].squeeze(), dim=0)
         pred = torch.argmax(predictions, dim=0)
@@ -102,6 +103,8 @@ class TopicClassifier(BaseHandler, ABC):
         return [data] # Return the data as is but in a list
 
 _service = TopicClassifier()
+
+logger.log(logging.INFO, f"{__name__} is Running")
 
 def handle(data, context):
     try:
